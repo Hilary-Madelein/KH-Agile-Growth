b30c2edae9dca87ca2bc24a6d6ddaefde418f8a2
Hilary-Madelein <hilary.calva@unl.edu.ec>
2025-01-29 10:43:02 -0500

    Correccion de relacion foranea

--- models/proyecto.js
+++ models/proyecto.js
@@ -20,32 +20,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
   proyecto.associate = function (models) {
-    proyecto.hasMany(models.rol_proyecto, {
-      foreignKey: "id_proyecto",
-      as: "proyecto_rol",
-    });
-    proyecto.hasMany(models.resultado_checklist, {
-      foreignKey: "id_proyecto",
-      as: "resultado_checklist",
-    });
+    proyecto.hasMany(models.rol_proyecto, { foreignKey: 'id_proyecto', as: 'proyecto_rol' });
+    proyecto.hasMany(models.resultado_checklist, { foreignKey: 'id_proyecto', as: 'resultado_checklist' });
+    proyecto.hasMany(models.resultado_categoria, { as: 'resultado_categoria', foreignKey: 'id_proyecto' });
+    proyecto.hasMany(models.nivel_madurez_general, { as: 'nivel_madurez_general', foreignKey: 'id_proyecto' });
   };
 
   return proyecto;
-    const proyecto = sequelize.define('proyecto', {
-        external_id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, unique: true},
-        estado:{type: DataTypes.BOOLEAN, defaultValue: true},
-        nombre: { type: DataTypes.STRING(40), allowNull: false},
-        fecha_inicio: { type: DataTypes.DATE},
-        descripcion: { type: DataTypes.STRING(350), allowNull: true }
-    }, {
-        freezeTableName: true
-    });
-    proyecto.associate = function (models){
-        proyecto.hasMany(models.rol_proyecto, {foreignKey: 'id_proyecto',as:'proyecto_rol'});
-        proyecto.hasMany(models.resultado_checklist, {foreignKey: 'id_proyecto',as:'resultado_checklist'});
-        proyecto.hasMany(models.resultado_categoria, { as: 'resultado_categoria', foreignKey: 'id_proyecto'});
-        proyecto.hasMany(models.nivel_madurez_general, { as: 'nivel_madurez_general', foreignKey: 'id_proyecto'});
-    };
- 
-    return proyecto;
 };
